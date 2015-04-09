3fe50bc6ed2815ddde1df56dbb9f9e1ff006f61f
MD4 <m.dequa@gmail.com>
2015-04-09 16:58:21 +0200

    Physics

--- physics.py
+++ physics.py
@@ -1,3 +1,18 @@
 __author__ = 'md4'
 
+import math
+
+class Physics:
+
+    def tractionForce(self, mc, mp, alpha, beta, g):
+        return (mc * g) * math.sin(beta) - (mp * g) * math.cos(alpha)
+
+    def momentum(self, F, L):
+        return F * L
+
+    def inertia(self, mb, L):
+        return (mb * (L ^ 2)) / 3
+
+    def velocity(self, a, L):
+        return a * L
 
